df2f2f2a7c682627806a46dced447ddf82e38fb2
Nobu <omabrirdu@yandex.ru>
2025-04-18 01:03:43 +0500

    fix: removed force unwrap

--- ImageFeed/OAuthService/constants.swift
+++ ImageFeed/OAuthService/constants.swift
@@ -5,5 +5,5 @@ enum Constants{
   static let secretKey = "WL_L6ZDjOGjBmDmgyBnIBLfGqbVgn2-fxDTLEcBt64M"
   static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
   static let accessScope = "public+read_user+write_likes"
-  static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+  static let defaultBaseURL: URL? = URL(string: "https://api.unsplash.com")
 }
