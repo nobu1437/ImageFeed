d3ac96ef3eb02f60d3f84d6f89c822619973ef19
Nobu <omabrirdu@yandex.ru>
2025-04-18 01:04:10 +0500

    fix: removed force unwrap

--- ImageFeed/OAuthService/OAuth.swift
+++ ImageFeed/OAuthService/OAuth.swift
@@ -4,40 +4,51 @@ final class OAuth2Service {
   static let shared = OAuth2Service()
   private init() {}
   
-  func makeOAuthTokenRequest(code: String) -> URLRequest {
-    let baseURL = URL(string: "https://unsplash.com") ?? Constants.defaultBaseURL
-    let url = URL(
+  func fetchOAuthToken(_ code:String,completion: @escaping(Result<String,Error>) -> Void){
+    switch  makeOAuthTokenRequest(code: code){
+    case .success(let request):
+      let task = URLSession.shared.data(for: request) { result in
+        switch result {
+        case .success(let data):
+          do {
+            let tokenResponse = try JSONDecoder().decode(OAuthTokenResponseBody.self, from: data)
+            OAuth2TokenStorage.shared.token = tokenResponse.accessToken
+            completion(.success(tokenResponse.accessToken))
+          } catch {
+            completion(.failure(error))
+            print("Failed to decode OAuth Token")
+          }
+        case .failure(let error):
+          completion(.failure(error))
+          print("Failed to fetch OAuth token")
+        }
+      }
+      task.resume()
+    case .failure(let error):
+      print("Error: cant create request \(error)")
+         completion(.failure(error))
+    }
+  }
+  
+  func makeOAuthTokenRequest(code: String) -> Result<URLRequest,UrlError> {
+    guard let baseURL = URL(string: "https://unsplash.com") else {
+      print("Error: invalid base URL")
+      return.failure(.invalidBaseURL)
+    }
+    guard let url = URL(
       string: "/oauth/token"
-      + "?client_id=\(Constants.accessKey)"        
+      + "?client_id=\(Constants.accessKey)"
       + "&&client_secret=\(Constants.secretKey)"
       + "&&redirect_uri=\(Constants.redirectURI)"
       + "&&code=\(code)"
       + "&&grant_type=authorization_code",
       relativeTo: baseURL
-    ) ?? Constants.defaultBaseURL
+    ) else{
+      print ("Error: invalid URL")
+      return .failure(.invalidURL)
+    }
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
-    return request
-  }
-  
-  func fetchOAuthToken(_ code:String,completion: @escaping(Result<String,Error>) -> Void){
-    let request = makeOAuthTokenRequest(code: code)
-    let task = URLSession.shared.data(for: request) { result in
-      switch result {
-      case .success(let data):
-        do {
-          let tokenResponse = try JSONDecoder().decode(OAuthTokenResponseBody.self, from: data)
-          OAuth2TokenStorage.shared.token = tokenResponse.accessToken
-          completion(.success(tokenResponse.accessToken))
-        } catch {
-          completion(.failure(error))
-          print("Failed to decode OAuth Token")
-        }
-      case .failure(let error):
-        completion(.failure(error))
-        print("Failed to fetch OAuth token")
-      }
-    }
-    task.resume()
+    return .success(request)
   }
 }
