5e59cf2eb3aba62201f717f0627701fc8786e6ec
Nobu <omabrirdu@yandex.ru>
2025-04-20 17:50:21 +0500

    fix: added tokenkey constant

--- ImageFeed/Services/OAuthService/OAuth2TokenStorage.swift
+++ ImageFeed/Services/OAuthService/OAuth2TokenStorage.swift
@@ -3,24 +3,24 @@ import SwiftKeychainWrapper
 
 public class OAuth2TokenStorage{
   static let shared = OAuth2TokenStorage()
-  
+  private let tokenKey = "accessToken"
   private init(){}
   
   private let storage = KeychainWrapper.standard
   
   var token:String?{
     get{
-      storage.string(forKey: "accessToken")
+      storage.string(forKey: tokenKey)
     }
     set{
       guard let value = newValue else {
         assertionFailure("cant store empty token")
         return
       }
-       let isSuccess = storage.set(value, forKey: "accessToken")
+       let isSuccess = storage.set(value, forKey: tokenKey)
       guard isSuccess else{
         print("error storing token, removing it")
-        storage.removeObject(forKey: "accessToken")
+        storage.removeObject(forKey: tokenKey)
         return
       }
     }
